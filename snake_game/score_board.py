5b7651bfccfa463f203fa94e428a91c8ff9e824c
AMO-SHADROU <106986563+AMO-SHADROU@users.noreply.github.com>
2024-03-29 23:45:20 +0330

    high_score added to snake game

--- snake_game/score_board.py
+++ snake_game/score_board.py
@@ -5,6 +5,9 @@ class ScoreBoard(turtle.Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", "r") as f:
+            data = f.readline()
+        self.high_score = int(data)
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -14,8 +17,16 @@ class ScoreBoard(turtle.Turtle):
     def update_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("arial", 20, "normal"))
+        self.write(f"Score: {self.score}, High Score: {self.high_score}", align="center", font=("arial", 20, "normal"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("arial", 20, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as f:
+                f.write(str(self.high_score))
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align="center", font=("arial", 20, "normal"))
