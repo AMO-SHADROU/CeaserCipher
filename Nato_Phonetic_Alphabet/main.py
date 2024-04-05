9c6076f06a8638d299c03a1479221c24a133377b
AMO-SHADROU <106986563+AMO-SHADROU@users.noreply.github.com>
2024-04-05 15:36:40 +0330

    Nato phonetic updated : Error handling

--- Nato_Phonetic_Alphabet/main.py
+++ Nato_Phonetic_Alphabet/main.py
@@ -3,7 +3,14 @@ import pandas
 df = pandas.read_csv('nato_phonetic_alphabet.csv')
 dict1 = {row.letter: row.code for index, row in df.iterrows()}
 
-user_word = input('Enter a word: ').upper()
-user_letter = [dict1[letter] for letter in user_word]
 
-print(user_letter)
+def generate_phonetic_alphabet():
+    user_word = input('Enter a word: ').upper()
+    try:
+        user_letter = [dict1[letter] for letter in user_word]
+    except KeyError:
+        print('Sorry, only letters are allowed')
+        generate_phonetic_alphabet()
+    else:
+        print(user_letter)
+
